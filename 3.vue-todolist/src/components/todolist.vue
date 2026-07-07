76741ae709b22da645546a6e6afdb07076ff4925
NicholasMiko <91362362+NicholasMiko@users.noreply.github.com>
2026-07-07 09:22:49 +0700

    Update todolist.vue
    Revisi untuk part 3: To Do List (Sebelum Delete)

--- 3.vue-todolist/src/components/todolist.vue
+++ 3.vue-todolist/src/components/todolist.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>TO DO LIST</h1>
+  <div class="form">
     <input
       type="text"
       v-model="input"
+      placeholder="Ketik disini..."
   >
   <button @click="tambah">
     Tambah
   </button>
+  </div>
   <ul>
     <li
       v-for="(todo, index) in todos"
@@ -52,38 +55,51 @@ h1 {
   text-align: center;
 }
 
-input[type="text"] {
-  display: block;
+li {
+  background: lightgray;
+  padding: 20px;
+  width: 300px;
   margin: 20px auto;
+  margin-top: 15px;
+  border-radius: 15px;
+
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.selesai {
+
+background: lightgreen;
+
+}
+/* Ko, ini dipisahin bungkus span nya dengan yg atas ya.
+kalau digabung hijau nya tidak full 1 bubble*/
+.selesai span{
+  
+     text-decoration: line-through;
+  color: gray;
+}
+
+.form {
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  margin: 20px 0;
+}
+
+input[type="text"] {
+  width: 250px;
   padding: 15px;
   border-radius: 10px;
   border: 2px solid grey;
 }
 
 button {
-  display: block;
-  margin: 20px auto;
   padding: 15px 30px;
   border: none;
   border-radius: 10px;
   background: green;
   color: white;
-}
-
-ul {
-  list-style: none;
-  padding: 0;
-  width: 300px;
-  margin: 20px auto;
-}
-
-li {
-  background: lightgray;
-  padding: 20px;
-  margin-top: 15px;
-  border-radius: 15px;
-}
-.selesai {
-  background: lightgreen;
 }
 </style>
